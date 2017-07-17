79167f381657845e31d821ca4bd7a237b5ff2419
Sittipol Khamrueangsri <ikkyu.on.blog@hotmail.com>
2017-07-18 02:34:42 +0700

    Initial Commit

---
+++ SoccerSquad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoccerSquad
+//
+//  Created by CSmacmini on 7/18/2560 BE.
+//  Copyright © 2560 Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
